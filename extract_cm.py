174609bd4c90c64a5d57c13c680ad8981c63b2e1
Rav2 <okazje137@gmail.com>
2021-12-02 16:21:59 +0100

    updating tb10 RPV results

--- extract_cm.py
+++ extract_cm.py
@@ -8,13 +8,13 @@ model = 'RPV'
 # mode = 'WHL_M2_mu'
 # rootS = '13'
 modes = [  
-        'BLR_mdif20',
-         'BHL_M1_mL',
-        'BHL_M1_mu',
-        'BHR_M1_mR',
-        'BHR_M1_mu',
-        'WHL_M2_mL',
-        'WHL_M2_mu',
+        #'BLR_mdif20',
+        # 'BHL_M1_mL',
+        #'BHL_M1_mu',
+        #'BHR_M1_mR',
+        #'BHR_M1_mu',
+        #'WHL_M2_mL',
+        #'WHL_M2_mu',
         # 'WHL_M2_mu_2',
     	'BLR_tb10',
         'BLR_tb50'
